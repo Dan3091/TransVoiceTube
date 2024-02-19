d02883a87ee949365941e9d26624d8f54a2ae126
Dan <clarc.2007@gmail.com>
2024-02-19 19:42:15 +0200

    Imported all necessary modules and creted a global variable txt for audio to text conversion

---
+++ audio_translate.py
@@ -0,0 +1,9 @@
+from pytube import YouTube
+import os
+from speech_recognition import *
+from pydub import AudioSegment
+from gtts import gTTS
+from deep_translator import GoogleTranslator
+
+
+txt = ""
