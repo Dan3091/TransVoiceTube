08e84b37afce8e028ed65a80b099854aafb583a0
Dan <clarc.2007@gmail.com>
2024-02-19 19:48:46 +0200

    Defined the download_video function that downloads the specified url video from youtube

--- audio_translate.py
+++ audio_translate.py
@@ -7,3 +7,18 @@ from deep_translator import GoogleTranslator
 
 
 txt = ""
+
+def download_video(url_video):
+    """
+    This function take the url as an argument download the audio
+    from the specified url video link and return True if it succeeds
+    or False otherwise.
+    """
+
+    try:
+        yt = YouTube(url_video)
+        stream = yt.streams.get_audio_only()
+        stream.download(filename="AudioTrack.mp4")
+        return True
+    except:
+        return False
