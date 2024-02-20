86758ed3123220ad9eac51921dea8677519d5f09
Dan <clarc.2007@gmail.com>
2024-02-20 17:26:30 +0200

    Created the main window

--- gui_audio_recognition.py
+++ gui_audio_recognition.py
@@ -1,2 +1,18 @@
 from tkinter import *
 import customtkinter
+
+
+class App(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("TransVoiceTube")
+        self.resizable(False, False)
+        self.configure(bg='#38454c')
+        self.overrideredirect(True)
+
+        # Calculation of the main window position
+        posx = str(self.winfo_screenwidth() // 2 - 500 // 2)
+        posy = str(self.winfo_screenheight() // 2 - 380 // 2)
+        self.geometry(f"500x380+{posx}+{posy}")
+
+App().mainloop()
