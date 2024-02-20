3958afe6dd0f165ee2bd93b7782700adc74e7b81
Dan <clarc.2007@gmail.com>
2024-02-20 18:33:48 +0200

    Defined the popup_message function

--- main.py
+++ main.py
@@ -66,6 +66,38 @@ def language_selector_validation():
         app.label_select_language_error.config(text="*The languages can't be the same")
         return False
 
+def popup_message(message):
+    """
+    This function creates a popup window message.
+    """
+
+    pop = Tk()
+    pop.attributes('-topmost', 1)
+    pop.grab_set()
+    pop.configure(bg="#242c31")
+    pop.resizable(False, False)
+    pop.overrideredirect(True)
+    pop_label = Label(pop, text=message, font="Italic 16", bg="#242c31", fg="#95b1bc")
+    pop_label.pack(pady=(10, 20), padx=10)
+    btn = Button(pop,
+                 text="OK",
+                 font="Unispace 14",
+                 background="#38454c",
+                 foreground="#a9babc",
+                 activebackground="#283f5b",
+                 activeforeground="white",
+                 width=10,
+                 command=lambda:[app.deiconify(), pop.destroy()])
+    btn.pack()
+
+    pop_label = Label(pop, text="", bg="#242c31")
+    pop_label.pack(pady=(10, 0), padx=10)
+    pop.update_idletasks()
+    posx = str((pop.winfo_screenwidth() // 2) - (pop.winfo_width() // 2))
+    posy = str((pop.winfo_screenheight() // 2) - (pop.winfo_height() // 2))
+    pop.geometry(f"{pop.winfo_width()}x{pop.winfo_height()}+{posx}+{posy}")
+    pop.mainloop()
+
 def data_processing():
     pass
 
