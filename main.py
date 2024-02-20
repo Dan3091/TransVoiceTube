3a4cf1ea94bf03ad1c2795f8a302d37c4ea013be
Dan <clarc.2007@gmail.com>
2024-02-20 18:08:37 +0200

    Defined the url_entry_validation function

--- main.py
+++ main.py
@@ -2,3 +2,29 @@ from tkinter import *
 from gui_audio_recognition import App
 from audio_translate import *
 import threading
+
+
+def url_entry_validation():
+    """
+    This function validates the url_entry, first checks if there are some text,
+    then checks if 'youtube.com' is present inside the text.
+    """
+
+    entry_url = app.entry_url.get()
+    if len(entry_url) > 0:
+        if "youtube.com" in entry_url:
+            app.label_url_error.config(text="")
+            return True
+        else:
+            app.label_url_error.config(text="*Insert the YouTube link")
+            return False
+    else:
+        app.label_url_error.config(text="*Insert the link")
+        return False
+
+def data_processing():
+    pass
+
+if __name__ == "__main__":
+    app = App(data_processing)
+    app.mainloop()
