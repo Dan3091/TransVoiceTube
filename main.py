117e60de6d41238b004484d3e4df0958e6276c21
Dan <clarc.2007@gmail.com>
2024-02-20 18:25:57 +0200

    Defined three functions language_from_validation, language_to_validation and language_selector_validation

--- main.py
+++ main.py
@@ -22,6 +22,50 @@ def url_entry_validation():
         app.label_url_error.config(text="*Insert the link")
         return False
 
+def language_from_validation():
+    """
+    The function validates the from_language selector, checks if
+    the language was selected or not.
+    """
+
+    language_from = app.clicked1.get()
+    if language_from != "Select":
+        app.label_translate_from_error.config(text="")
+        return True
+    else:
+        app.label_translate_from_error.config(text="*Select language")
+        return False
+
+
+def language_to_validation():
+    """
+    The function validates the to_language selector, checks if
+    the language was selected or not.
+    """
+
+    language_to = app.clicked2.get()
+    if language_to != "Select":
+        app.label_translate_to_error.config(text="")
+        return True
+    else:
+        app.label_translate_to_error.config(text="*Select language")
+        return False
+
+
+def language_selector_validation():
+    """
+    This function checks if the two selected languages are the same.
+    """
+
+    language_from = app.clicked1.get()
+    language_to = app.clicked2.get()
+    if language_from != language_to or language_from == "Select" and language_to == "Select":
+        app.label_select_language_error.config(text="")
+        return True
+    else:
+        app.label_select_language_error.config(text="*The languages can't be the same")
+        return False
+
 def data_processing():
     pass
 
